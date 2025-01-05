b3eac69154ffc0d792aa4c859e945a89b917acb3
xddbom <xddbom@gmail.com>
2025-01-05 21:40:22 +0200

    Add server start logic

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"github.com/xddbom/cache-api/db"
+    "github.com/gin-gonic/gin"
 )
 
 func main() {    
     db.RedisInit()
+
+    r := gin.Default()
+
+    r.GET("/", func(c *gin.Context){
+        c.JSON(200, gin.H{
+            "message": "Welcome to Cache API!",
+        })
+    })
+
+    r.Run(":8080")
 }
